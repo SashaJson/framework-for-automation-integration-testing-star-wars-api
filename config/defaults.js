f96fb9c9e16705cd47b2e9b93bd0b25d923c5c19
SashaJson <sashamiller666@gmail.com>
2021-01-22 11:33:04 +0200

    updated config/default.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,16 +1,22 @@
-"use strict";
+'use strict';
 
 module.exports = {
 
     //---------------Config for testing API---------------
-    URL: "https://swapi.dev/api",
+
+    URL: 'https://swapi.dev/api',
+
 
     //---------------Configurations Headers---------------
-    contentType: 'application/json',
+
+    CONTENT_TYPE_JSON: 'application/json',
+
 
     //---------------Config for status Code and Text---------------
-    code200: 200,
-    statusTextOk: 'OK',
-    code404: 404,
-    statusTextNotFound: 'NOT FOUND'
+
+    CODE_200: 200,
+    STATUS_TEXT_OK: 'OK',
+    CODE_404: 404,
+    STATUS_TEXT_NOT_FOUND: 'NOT FOUND'
+
 }
