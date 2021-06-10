2f510649ab2fbaa25c842fdb3eceb4f21a08b3b0
SashaJson <sashamiller666@gmail.com>
2021-06-11 02:46:54 +0300

    changed validationJsonSchemas.js

--- helpers/validationJsonSchemas.js
+++ helpers/validationJsonSchemas.js
@@ -1,32 +1,37 @@
 'use strict';
 
-const Ajv = require('ajv'),
-    ajv = new Ajv.default({ ajvErrors: true, strict: true }),
-    inspect = require('util').inspect;
+const Ajv = require('ajv').default,
+    ajv = new Ajv();
+
+const inspect = require('util').inspect;
 
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
 ajv.addSchema(require('../json-schemas/definitions.json'));
 ajv.addSchema(require('../json-schemas/models-json-schema/root.json'));
 ajv.addSchema(require('../json-schemas/models-json-schema/people.json'));
 
-module.exports = (jsonSchemaReceivedFromServer, validJsonSchema) => {
+module.exports = class ValidationJsonSchemas {
 
-    const validate = ajv.compile(validJsonSchema);
-    const valid = validate(jsonSchemaReceivedFromServer)
+     validation(jsonSchemaReceivedFromServer, validJsonSchema) {
 
-    if (!valid) {
-        throw new Error('Json Schema validation error. Details: ' +
-            JSON.stringify({ validationError: validate.errors }, null, 2) + '\n' +
-            'Json Schema with Error from server: ' + inspect(jsonSchemaReceivedFromServer, {
-                showHidden: false,
-                depth: null
-            }));
+        const validate = ajv.compile(validJsonSchema);
+        const valid = validate(jsonSchemaReceivedFromServer)
+
+        if (!valid) {
+            throw new Error('Json Schema validation error. Details: ' +
+                JSON.stringify({ validationError: validate.errors }, null, 2) + '\n' +
+                'Json Schema with Error from server: ' + inspect(jsonSchemaReceivedFromServer, {
+                    showHidden: false,
+                    depth: null
+                }));
+        }
+
+        console.log('Json Schema from server: ' + inspect(jsonSchemaReceivedFromServer, {
+            showHidden: false,
+            depth: null,
+            colors: true
+        }));
+
     }
-
-    console.log('Json Schema from server: ' + inspect(jsonSchemaReceivedFromServer, {
-        showHidden: false,
-        depth: null,
-        colors: true
-    }));
 
 }
