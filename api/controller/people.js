d26a471d8c4e94a5b1f2c89e28f569a03b5733d4
SashaJson <sashamiller666@gmail.com>
2021-06-11 02:48:55 +0300

    changed in controller people

--- api/controller/people.js
+++ api/controller/people.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const starWarsAPI = require('../requesterStarWarsAPI');
-const validateJson = require('../../helpers/validationJsonSchemas');
+const StarWarsAPI = require('../requesterStarWarsAPI');
+const ValidationJsonSchemas = require('../../helpers/validationJsonSchemas');
 
 module.exports = class PeopleController {
 
     constructor() {
-        this._starWarsApi = new starWarsAPI;
+        this._starWarsApi = new StarWarsAPI;
+        this.ajv = new ValidationJsonSchemas();
     }
 
     async getAllPeople() {
-        const bla = this._starWarsApi.get('https://swapi.dev/api/people/schema');
-        console.log(bla)
+        this.ajv.validation(await this._starWarsApi.get('/people/schema'), {});
     }
 
 }
